1f1c51cc6592074e2bc4bf97b4f7a65458ad3ab2
chandra <sekar_vishaal@yahoo.co.in>
2022-02-06 23:14:14 +0530

    fourth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 import Layout from './components/Layout/Layout'
-
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
   
